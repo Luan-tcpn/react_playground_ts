019eab71a77082f83278a997722b2b0159211f4f
Luan-tcpn <luanoliveiraa200@gmail.com>
2025-02-20 16:21:26 -0300

    props e hooks

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Home from './pages/home/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home
+        titulo="Componente Home"
+        texto="Este é o componente Home, que recebe props."
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+
+interface homeProps {
+  titulo: string;
+  texto: string;
+}
+
+function Home(props: homeProps) {
+  return (
+      <div>
+          <h2>{props.titulo}</h2>
+          <p>{props.texto}</p>
+      </div>
+  );
+  
+}
+
+export default Home;
